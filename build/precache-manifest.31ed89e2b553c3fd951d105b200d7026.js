e8400d4fce7a14410bc32b6a6fa63c4b336e5737
fboisier <francisco.boisier.sandoval@gmail.com>
2020-10-01 12:04:46 -0300

    se ejecuta el build

---
+++ build/precache-manifest.31ed89e2b553c3fd951d105b200d7026.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f38306c5e4271f0c9c22792804a9e313",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cdf445ae4e4aaa40dc28",
+    "url": "/static/css/main.6184bf9e.chunk.css"
+  },
+  {
+    "revision": "9b9c730fb828647cb89d",
+    "url": "/static/js/2.0d69d3f5.chunk.js"
+  },
+  {
+    "revision": "17061da1f707efaaf35913ea934465ff",
+    "url": "/static/js/2.0d69d3f5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdf445ae4e4aaa40dc28",
+    "url": "/static/js/main.a116314d.chunk.js"
+  },
+  {
+    "revision": "eda2a4fdf3a474668242",
+    "url": "/static/js/runtime-main.9611f50a.js"
+  }
+]);
